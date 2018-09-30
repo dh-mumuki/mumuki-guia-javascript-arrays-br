aa732c8aa88c2c4e03387ec6a3d8ade7fac60715
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-29 20:36:07 -0400

    Mumuki Export on 2018-09-29 20:36:07 -0400

--- 00003_E isso é uma matriz/extra.js
+++ 00003_E isso é uma matriz/extra.js
@@ -1,7 +1,7 @@
 var personas = ["mara", "julian"];
 
 function arraysIguais (unArray, otroArray) {
-if (unArray.length! = otroArray.length)
+if (unArray.length != otroArray.length)
     return false;
 for (var i = 0; i <unArray.length; i ++) {
     if (unArray [i] instanceOf Array && otroArray [i] instanceOf Array) {

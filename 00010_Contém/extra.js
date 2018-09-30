ebed7952c6e6b55a96907698d49f0ab88fb81b3c
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-29 21:10:00 -0400

    Mumuki Export on 2018-09-29 21:10:00 -0400

--- 00010_Contém/extra.js
+++ 00010_Contém/extra.js
@@ -1,20 +1,20 @@
-describe ("", () => {
-  it ("contem ([1, 6, 7, 6], 7) é verdadeiro", () => {
-assert (contém ([1, 6, 7, 6], 7));
+describe("", () => {
+  it("contem([1, 6, 7, 6], 7) es true", () => {
+    assert(contem([1, 6, 7, 6], 7));
   })
-  it ("contem ([1, 6, 7, 6], 6) é verdadeiro", () => {
-assert (contem ([1, 6, 7, 6], 6));
+  it("contem([1, 6, 7, 6], 6) es true", () => {
+    assert(contem([1, 6, 7, 6], 6));
   })
- it ("contem ([], 7) é falso", () => {
-assert (! contem ([], 7));
+  it("contem([], 7) es false", () => {
+    assert(!contem([], 7));
   })
-  it ("contem ([8, 5], 7) é falso", () => {
-assert (! contem ([8, 5], 7));
+  it("contem([8, 5], 7) es false", () => {
+    assert(!contem([8, 5], 7));
   })
-  it ("contem ([1, 8, 5], 7) é falso", () => {
-assert (! contem ([1, 8, 5], 7));
+  it("contem([1, 8, 5], 7) es false", () => {
+    assert(!contem([1, 8, 5], 7));
   })
-  it ("contem ([1, 1, 1], 1) é verdadeiro", () => {
-assert (contem ([1, 1, 1], 1));
+  it("contem([1, 1, 1], 1) es true", () => {
+    assert(contem([1, 1, 1], 1));
   })
 })

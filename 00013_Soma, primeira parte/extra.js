2877fa6e315727490d7e4a853d2142c3917aa7f5
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-29 21:21:20 -0400

    Mumuki Export on 2018-09-29 21:21:20 -0400

--- 00013_Soma, primeira parte/extra.js
+++ 00013_Soma, primeira parte/extra.js
@@ -1,5 +1,5 @@
-var gananciasUltimoSemestre = [50, -12, 1000, 300, 200, 0];
-var gananciasPrimerTrimestre = [80, 453, 1000];
+var lucroUltimoSemestre = [50, -12, 1000, 300, 200, 0];
+var lucrosPrimeiroTrimestre = [80, 453, 1000];
 
 function somaLucroSemestre(unSemestre) {
   return unSemestre[0] + unSemestre[1] +

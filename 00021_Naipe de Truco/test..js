0294a48587fd16693a707d51887c5d944a0fe90e
andres@digitalhouse.com <andres@digitalhouse.com>
2018-09-30 17:25:30 -0400

    Mumuki Export on 2018-09-30 17:25:30 -0400

--- 00021_Naipe de Truco/test..js
+++ 00021_Naipe de Truco/test..js
@@ -1,17 +1,17 @@
 describe("", function() {
-  var paloAlAzar =  Math.random().toString(36).substring(2);
+  var naipeDeAzar =  Math.random().toString(36).substring(2);
 
-  it('paloDeTruco("espadas") NO debería contener el "8 de espadas"', function() {
-    assert(paloDeTruco("espadas").indexOf("8 de espadas") < 0);
+  it('naipeDeTruco("espadas") NÃO deveria conter o "8 de espadas"', function() {
+    assert(naipeDeTruco("espadas").indexOf("8 de espadas") < 0);
   });
 
-  it('paloDeTruco("espadas") NO debería contenerel "9 de espadas"', function() {
-    assert(paloDeTruco("espadas").indexOf("9 de espadas") < 0);
+  it('naipeDeTruco("espadas") NÃO deveria conter o "9 de espadas"', function() {
+    assert(naipeDeTruco("espadas").indexOf("9 de espadas") < 0);
   });
 
-  it('paloDeTruco("espadas") debería ser ["1 de espadas", "2 de espadas"...', function() {
+  it('naipeDeTruco("espadas") deveria ser ["1 de espadas", "2 de espadas"...', function() {
     assert.deepEqual(
-      paloDeTruco("espadas"),
+      naipeDeTruco("espadas"),
       [ '1 de espadas',
       '2 de espadas',
       '3 de espadas',
@@ -24,22 +24,22 @@ describe("", function() {
       '12 de espadas' ]);
   });
 
-  it('paloDeTruco("bastos") debería ser ["1 de bastos", "2 de bastos"...', function() {
+  it('naipeDeTruco("paus") deveria ser ["1 de paus", "2 de paus"...', function() {
     assert.deepEqual(
-      paloDeTruco("bastos"),
-      [ '1 de bastos',
-      '2 de bastos',
-      '3 de bastos',
-      '4 de bastos',
-      '5 de bastos',
-      '6 de bastos',
-      '7 de bastos',
-      '10 de bastos',
-      '11 de bastos',
-      '12 de bastos' ]);
+      naipeDeTruco("paus"),
+      [ '1 de paus',
+      '2 de paus',
+      '3 de paus',
+      '4 de paus',
+      '5 de paus',
+      '6 de paus',
+      '7 de paus',
+      '10 de paus',
+      '11 de paus',
+      '12 de paus' ]);
   });
 
-  it(`paloDeTruco("${paloAlAzar}") deberia tener el "3 de ${paloAlAzar}"`, function() {
-    assert(paloDeTruco(paloAlAzar).indexOf(`3 de ${paloAlAzar}`) > 0);
+  it(`naipeDeTruco("${naipeDeAzar}") deveria ter o "3 de ${naipeDeAzar}"`, function() {
+    assert(naipeDeTruco(naipeDeAzar).indexOf(`3 de ${naipeDeAzar}`) > 0);
   });
 })

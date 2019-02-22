273e2466cbce888185c8c19092675dc4141b5127
apereira@digitalhouse.com <apereira@digitalhouse.com>
2019-02-22 16:45:11 +0000

    Mumuki Export on 2019-02-22 16:45:11 +0000

--- 00010_Contém/extra.js
+++ 00010_Contém/extra.js
@@ -8,13 +8,13 @@ describe("", () => {
   it("contem([], 7) é false", () => {
     assert(!contem([], 7));
   })
-  it("contem([8, 5], 7) es false", () => {
+  it("contem([8, 5], 7) é false", () => {
     assert(!contem([8, 5], 7));
   })
-  it("contem([1, 8, 5], 7) es false", () => {
+  it("contem([1, 8, 5], 7) é false", () => {
     assert(!contem([1, 8, 5], 7));
   })
-  it("contem([1, 1, 1], 1) es true", () => {
+  it("contem([1, 1, 1], 1) é true", () => {
     assert(contem([1, 1, 1], 1));
   })
 })
